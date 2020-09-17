d75d76f9c60ecf5b335fbafc2599282b544126bc
Kegbi kegbi.networking@gmail.com <kegbi.networking@gmail.com>
2020-09-18 01:36:31 +0300

    Basic backend stuff

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+
+app.use(express.json({extended: true}))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
